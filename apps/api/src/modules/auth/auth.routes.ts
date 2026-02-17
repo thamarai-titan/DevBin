dc91f3ff2b5dca609591ed024ba483758d480088
Thamarai <tmanalan710@gmail.com>
2026-02-17 23:35:12 +0530

    route for register

--- apps/api/src/modules/auth/auth.routes.ts
+++ apps/api/src/modules/auth/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { RegisterController } from "./auth.controller";
 
 const router = Router()
 
 
-router.post("/auth/register", )
+router.post("/auth/register", RegisterController)
 
 
 export default router
