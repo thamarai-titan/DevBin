9d83b8c886089a358ed809633b088c80c1ae8e16
Thamarai <tmanalan710@gmail.com>
2026-02-18 14:03:17 +0530

    Register endpoint completed

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express"
 import { RegisterSchema, type RegisterType } from "./auth.schema"
 import { checkEmail, RegisterService } from "./auth.service"
+import { responses } from "../../lib/responses"
 
 export const RegisterController = async (req:Request, res:Response)=> {
     try {
@@ -8,10 +9,11 @@ export const RegisterController = async (req:Request, res:Response)=> {
         const emailsExsists = await checkEmail(data.email)
 
         if(emailsExsists){
-            
+            res.status(400).json(responses.error("EMAIL ALREADY EXSISTS"))
         }
         const user = await RegisterService(data)
 
+        res.status(201).json(responses.success(user))
 
     } catch (error) {
         
