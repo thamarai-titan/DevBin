f0d0d202fb61324e3767c3800e70d67a79d9234c
Thamarai <tmanalan710@gmail.com>
2026-02-19 22:23:28 +0530

    auth middleware added

---
+++ apps/api/src/middleware/auth.middleware.ts
@@ -0,0 +1,35 @@
+import type {Request, Response, NextFunction} from "express"
+import { responses } from "../lib/responses";
+import jwt from "jsonwebtoken"
+
+declare global {
+    namespace Express {
+        interface Request {
+            userId: string,
+            role: string
+        }
+    }
+}
+
+export const verifyToken = async (req: Request, res: Response, next: NextFunction)=>{
+    const authToken = req.headers.authorization;
+
+    if(!authToken || !authToken.startsWith("Bearer")){
+        return res.status(401).json(responses.error("UNAUTHORIZED"))
+    }
+
+    const token = authToken.split(" ")[1] as string
+
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY!) as {
+            userId: string,
+            role: string
+        }
+        req.userId = decoded.userId
+        req.role = decoded.role
+        next()
+
+    } catch (error) {
+        return res.status(401).json(responses.error("UNAUTHORIZED"))
+    }
+}
