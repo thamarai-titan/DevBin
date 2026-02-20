966f81805adc87d8f792b593a5a2b9af50c003f6
Thamarai <tmanalan710@gmail.com>
2026-02-20 22:51:41 +0530

    routes for saved tools added

--- apps/api/src/modules/tools/tools.routes.ts
+++ apps/api/src/modules/tools/tools.routes.ts
@@ -1,10 +1,24 @@
 import { Router } from "express";
-import { verifyToken } from "../../middleware/auth.middleware";
-import { GetallToolsController, GetToolController } from "./tools.controller";
+import {
+  CreatetoolController,
+  DeleteToolController,
+  GetallToolsController,
+  GetToolController,
+  SaveToolController,
+  UnsaveToolController,
+} from "./tools.controller";
+import { verifyAdmin, verifyToken } from "../../middleware/auth.middleware";
 
-const router = Router()
+const router = Router();
 
-router.get("/tools", GetallToolsController)
-router.get("/tools/:id", GetToolController)
+router.get("/tools", GetallToolsController);
+router.get("/tools/:id", GetToolController);
+router.post("/tools", verifyToken,verifyAdmin,CreatetoolController);
 
-export default router
+
+router.delete("/tools/:id", verifyToken, verifyAdmin,  DeleteToolController);
+router.post("/saved", verifyToken, SaveToolController);
+router.delete("/saved/:toolId", UnsaveToolController);
+router.get("/saved", GetallToolsController);
+
+export default router;
