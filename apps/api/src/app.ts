683d2bc94815cd5172205091fb446bf05fdc5144
Thamarai <tmanalan710@gmail.com>
2026-02-20 22:03:26 +0530

    Added user view tools routes

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -2,6 +2,7 @@ import express from "express"
 import type { Request, Response } from "express"
 import cors from "cors"
 import authRouter from "./modules/auth/auth.routes.ts"
+import toolsRouter from "./modules/tools/tools.routes.ts"
 
 const app = express()
 
@@ -18,5 +19,6 @@ app.get("/", (req: Request, res: Response)=>{
 })
 
 app.use("/api", authRouter)
+app.use("/api", toolsRouter)
 
 export default app
