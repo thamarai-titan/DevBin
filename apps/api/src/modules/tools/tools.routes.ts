970834251e2c4029241ddca5afd3b193509bc307
Thamarai <tmanalan710@gmail.com>
2026-02-20 22:04:42 +0530

    routes for tools completed

--- apps/api/src/modules/tools/tools.routes.ts
+++ apps/api/src/modules/tools/tools.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyToken } from "../../middleware/auth.middleware";
-import { GetallToolsController } from "./tools.controller";
+import { GetallToolsController, GetToolController } from "./tools.controller";
 
 const router = Router()
 
 router.get("/tools", GetallToolsController)
+router.get("/tools/:id", GetToolController)
 
 export default router
