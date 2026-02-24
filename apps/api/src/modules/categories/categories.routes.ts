941d41d0f6ff149cc5c5a5f2434131485fd5e47b
Thamarai <tmanalan710@gmail.com>
2026-02-24 20:56:57 +0530

    categories routes added

--- apps/api/src/modules/categories/categories.routes.ts
+++ apps/api/src/modules/categories/categories.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { verifyAdmin, verifyToken } from "../../middleware/auth.middleware";
+import { CreateCategoriesController } from "./categories.controller";
 
 const router = Router();
 
-
-
+router.post("/categories", verifyToken, verifyAdmin, CreateCategoriesController)
 export default router
