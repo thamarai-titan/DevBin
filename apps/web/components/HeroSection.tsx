b2011adcd2d081f497646e4599d158da8c53e9c1
Thamarai <tmanalan710@gmail.com>
2026-03-14 22:03:15 +0530

    creating hero section

---
+++ apps/web/components/HeroSection.tsx
@@ -0,0 +1,10 @@
+
+
+export const HeroSection = () => {
+    return (
+        <div className="max-w-full">
+            <div className="w-full h-screen ">
+            </div>
+        </div>
+    )
+}
