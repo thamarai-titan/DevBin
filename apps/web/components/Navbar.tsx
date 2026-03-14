4494f4f14a0bf52064683dc2fc5c18298c47c733
Thamarai <tmanalan710@gmail.com>
2026-03-14 22:03:32 +0530

    navbar added

--- apps/web/components/Navbar.tsx
+++ apps/web/components/Navbar.tsx
@@ -1,7 +1,17 @@
+import { Menu } from "lucide-react"
+
+
 export const Navbar = () => {
     return (
-        <nav>
-            
+        <nav className="w-full p-10 z-50 top-0 fixed"> 
+            <div className="max-w-7xl mx-auto rounded-md border border-black w-40 p-3 flex justify-between items-center shadow-(--shadow-card)">
+                <div className="text-md font-bold">
+                    DevBin.
+                </div>
+                <div>
+                    <Menu className="w-5"/>
+                </div>
+            </div>
         </nav>
     )
 }
