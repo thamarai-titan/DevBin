6277dc176127360e7c0891ff2f822ae67884cb81
Thamarai <tmanalan710@gmail.com>
2026-03-15 22:06:53 +0530

    navbar creating

--- apps/web/components/Navbar.tsx
+++ apps/web/components/Navbar.tsx
@@ -1,17 +1,23 @@
-import { Menu } from "lucide-react"
+"use client"
 
+import { Menu } from "lucide-react"
+import { motion } from "motion/react"
 
 export const Navbar = () => {
     return (
         <nav className="w-full p-10 z-50 top-0 fixed"> 
-            <div className="max-w-7xl mx-auto rounded-md border border-black w-40 p-3 flex justify-between items-center shadow-(--shadow-card)">
+            <motion.div 
+            initial={{scale: 0}}
+            animate={{scale: 1}}
+            whileHover={{scale: 1.1}}
+            className="max-w-7xl mx-auto rounded-md border border-gray-300 w-40 p-3 flex justify-between items-center shadow-(--shadow-card)">
                 <div className="text-md font-bold">
                     DevBin.
                 </div>
                 <div>
                     <Menu className="w-5"/>
                 </div>
-            </div>
+            </motion.div>
         </nav>
     )
 }
