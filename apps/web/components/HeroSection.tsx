f23602f005a320cdb44991aa9a13c5e6fd4f8483
Thamarai <tmanalan710@gmail.com>
2026-03-16 21:01:27 +0530

    hero section added

--- apps/web/components/HeroSection.tsx
+++ apps/web/components/HeroSection.tsx
@@ -1,10 +1,48 @@
+"use client"
+
+import { ArrowDown } from "lucide-react"
+import { motion } from "motion/react"
 
 
 export const HeroSection = () => {
-    return (
-        <div className="max-w-full">
-            <div className="w-full h-screen ">
+    return <>
+        <section className="w-full min-h-screen flex items-center px-6 sm:px-10 md:px-16 bg-hero)"
+            style={{
+                backgroundImage: 'linear-gradient(90deg, rgb(255,255,255) 20%,transparent 90%),repeating-linear-gradient(135deg, rgba(0,0,0, 0.1) 0px, rgba(0,0,0, 0.1) 1px,transparent 1px, transparent 13px),repeating-linear-gradient(45deg, rgba(0,0,0, 0.1) 0px, rgba(0,0,0, 0.1) 1px,transparent 1px, transparent 13px),linear-gradient(90deg, rgb(255,255,255),rgb(255,255,255))'
+            }}
+        >
+            <div className="max-w-7xl mx-auto">
+                <div className="p-2 rounded inline-block my-2 font-light"># Most Usefull Tools</div>
+                <h1 className="text-6xl md:text-7xl font-bold max-w-4xl">
+                    Discover the
+                    <span className="marker text-(--color-accent)"> tools </span>
+                    that power the
+                    <span className="marker text-(--color-accent)"> internet </span>
+                </h1>
+                <p className="my-4 sm:my-4 md:my-10 text-sm sm:text-base md:text-md text-(--color-muted) max-w-xl leading-relaxed">
+                    Ever wondered what's really powering your favorite websites?
+                    Dive into the tools, technologies, and platforms
+                    that make the internet work.
+                </p>
+                <div className="border border-gray-200 p-2 mt-10 flex w-xs justify-between rounded ">
+                    <p>scroll to view</p>
+                    <motion.div
+                        initial={{ y: -2 }}
+                        animate={{ y: 2 }}
+                        transition={{
+                            repeat: Infinity,
+                            duration: 2,
+                            repeatType: "reverse",
+                            ease: "easeInOut",
+                        }}
+                        exit={{ y: -2 }}
+                    >
+                        <ArrowDown className="w-5 text-(--color-accent)"
+
+                        />
+                    </motion.div>
+                </div>
             </div>
-        </div>
-    )
+        </section>
+    </>
 }
