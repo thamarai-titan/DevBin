1fe5a0d924b989f5d36424ccfbee82aff36d0669
Thamarai <tmanalan710@gmail.com>
2026-03-24 22:38:59 +0530

    framer variants for nac

---
+++ apps/web/lib/framer/variants.ts
@@ -0,0 +1,15 @@
+export const navVariants = {
+    full: {
+        y: 0,
+        width: "100%",
+        left: "50%",
+        x: "-50%",
+        paddingTop: "2.5rem",
+        paddingBottom: "1.5rem",
+        opacity: 1,
+    },
+    hidden: {
+        y: "-100%",
+        opacity: 0,
+    }
+}
