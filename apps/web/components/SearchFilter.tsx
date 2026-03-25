47b9b40597a4fd77b123c8a5a3440626b0dda9d8
Thamarai <tmanalan710@gmail.com>
2026-03-25 22:15:07 +0530

    Search filter modified

--- apps/web/components/SearchFilter.tsx
+++ apps/web/components/SearchFilter.tsx
@@ -5,31 +5,32 @@ import { SearchFilterbarProps } from "@/types"
 
 const SearchFilter = ({
   searchQuery,
-  onCategoryChange,
+  onSearchChange,
   selectedCategory,
-  onSearchChange
+  onCategoryChange
 }: SearchFilterbarProps) => {
   return <>
-    <section className='w-full bg-[#0d0b0f]/95 backdrop-blur-2xl'>
+    <section className='w-full bg-(--color-bg) backdrop-blur-2xl'>
       <div className='max-w-7xl mx-auto p-6'>
         {/* search input */}
-        <div className='border border-(--color-muted) flex items-center rounded p-2 text-[#e8e8e8]'>
+        <div className='border border-(--color-border) flex items-center rounded p-2 text-(--color-text)'>
           <Search size={20}
             className="mr-2"
           />
           <input type="text"
             value={searchQuery}
             onChange={(e) => onSearchChange(e.target.value)}
-            className="text-white placeholder-[#e8e8e8] w-full outline-none"
+            className="text-(--color-text) placeholder-(--color-text) w-full outline-none"
             placeholder="Search here!"
           />
         </div>
         {/* category chips */}
         <div className="flex gap-3 overflow-x-auto py-4">
           {categories.map((category) => (
-            <button key={category.id}
+            <button 
+              key={category.id}
               onClick={() => onCategoryChange(category.id)}
-              className={`text-[#e8e8e8] px-4 py-2 rounded-full whitespace-nowrap border border-(--color-muted) ${selectedCategory === category.id} ? 'bg-white text-black' : ''`}
+              className={`text-(--color-text) px-4 py-2 rounded-full whitespace-nowrap border border-(--color-border) ${selectedCategory === category.id ? 'bg-white text-black' : ''}`}
             >
               {category.label}
             </button>
